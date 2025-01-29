dfcc4dc88473ccf18cd5a75812b85d7980f835ff
07204462 <07204462@dwc-legazpi.edu>
2025-01-29 17:10:51 +0800

    third commit

--- src/app/post/post.create/post.create.component.ts
+++ src/app/post/post.create/post.create.component.ts
@@ -1,11 +1,20 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { MatButton, MatButtonModule } from '@angular/material/button';
+import { MatInput, MatInputModule } from '@angular/material/input';
 
 @Component({
   standalone: true,
+  imports: [FormsModule, MatInputModule, MatButtonModule], 
   selector: 'post-create',
   templateUrl: './post.create.component.html',
   styleUrls: ['./post.create.component.css']
 })
 export class PostCreateComponent {
+  enteredValue = 'Test'
+  newPost = ''
+    onAddPost(){
+      this.newPost = this.enteredValue
 
+  }
 }
