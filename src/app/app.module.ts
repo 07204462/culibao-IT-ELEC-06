e385326418d50f3680df935cfcc0703a9d757349
07204462 <07204462@dwc-legazpi.edu>
2025-01-31 19:54:57 +0800

    fourth commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,22 +5,33 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FormsModule } from '@angular/forms';
-import { PostCreateComponent } from './post/post.create/post.create.component';
-import {MatInputModule} from '@angular/material/input';
-import {MatButtonModule} from '@angular/material/button';
+import { PostCreateComponent } from './post/post-create/post-create.component';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatExpansionModule} from '@angular/material/expansion';
+import { PostListComponent } from './post/post-list/post-list.component';
+import { MatToolbarModule} from '@angular/material/toolbar';
+import { HeaderComponent } from './header/header.component';
+import { MatIconModule } from '@angular/material/icon';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PostCreateComponent,
+    PostListComponent,
+    HeaderComponent
   ],
   imports: [
-    MatButtonModule,
-    MatInputModule,
     FormsModule,
-    PostCreateComponent,
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatExpansionModule,
+    MatButtonModule,
+    MatInputModule,
+    MatToolbarModule,
+    MatIconModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/post/post-create/post-create.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'post-create',
+  templateUrl: './post-create.component.html',
+  styleUrls: ['./post-create.component.css']
+})
+export class PostCreateComponent {
+  enteredValue = 'Test'
+  newPost = ''
+    onAddPost(){
+      this.newPost = this.enteredValue
+  }
+}
