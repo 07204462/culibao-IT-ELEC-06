3b3f804ca2662fd7c806b2d66e57bf54f439ae35
07204462 <07204462@dwc-legazpi.edu>
2025-02-25 13:46:29 +0800

    fourth commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FormsModule } from '@angular/forms';
-import { PostCreateComponent } from './post/post-create/post-create.component';
+
+import { PostCreateComponent } from './posts/post-create/post-create.component';
+import { PostListComponent } from './posts/post-list/post-list.component';
+import { HeaderComponent } from './header/header.component';
+
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
 import { MatExpansionModule} from '@angular/material/expansion';
-import { PostListComponent } from './post/post-list/post-list.component';
 import { MatToolbarModule} from '@angular/material/toolbar';
-import { HeaderComponent } from './header/header.component';
 import { MatIconModule } from '@angular/material/icon';
+import { MatCardModule } from '@angular/material/card';
 
 
 @NgModule({
@@ -27,11 +29,13 @@ import { MatIconModule } from '@angular/material/icon';
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
+    
     MatExpansionModule,
     MatButtonModule,
     MatInputModule,
     MatToolbarModule,
     MatIconModule,
+    MatCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
